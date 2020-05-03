945c2f2456c26f392eb8a71217eddf1d39850145
Nicholas Stock <nick@gembani.com>
2020-05-03 09:52:13 +0200

    finish add .env

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+gem 'dotenv'
 gem 'discordrb-webhooks'
 gem 'byebug'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
+require 'dotenv/load'
 require 'sinatra'
 require 'discordrb/webhooks'
-WEBHOOK_URL='https://discordapp.com/api/webhooks/706072018680020996/qjA1BvICOK8kTGe6-7QPudI3owzRgwtZvehLgmbOazPOEhPNAN0vti8_b1nCnmGNj5n4'
 post '/pivotal' do
   data = JSON.parse(request.body.read)
-  client = Discordrb::Webhooks::Client.new(url: WEBHOOK_URL)
+  client = Discordrb::Webhooks::Client.new(url: ENV.fetch('DISCORD_WEBHOOK'))
   client.execute do |builder|
     builder.content = data['message']
     data["primary_resources"].each do |resource|     
